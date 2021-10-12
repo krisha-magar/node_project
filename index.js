f24b347e6f4c9415501d0c6909479b0655df96e2
Krisha <krisha.magar77@gmail.com>
2021-10-12 09:19:20 +0545

    Changes made in index.js file

--- index.js
+++ index.js
@@ -27,8 +27,39 @@ app.get('/users/:id',(req, res) => {
   res.json(user);
 })
 
+//update user
+app.put("/users/:id",(req, res) => {
+  let userIndex = users.findIndex((user) => user.id === parseInt(req.params.id));
+  // // to know the index 
+  // res.json({
+  //   index: userIndex,
+  // })
+  if(userIndex === -1) {
+    return res.status(404).json({
+      error: "The user with the given id was not found",
+    });
+  }
+  //users[userIndex] = req.body;
+  users[userIndex]["name"] = req.body.name;
+  users[userIndex]["email"] = req.body.email;
+  users[userIndex]["address"] = req.body.address;
+  res.json(req.body)
+});
 
+//delete user by
+app.delete("/users/:id",(req, res) => {
+  let userIndex = users.findIndex(
+    (user) => user.id === parseInt(req.params.id)
+  );
+  if (userIndex === -1) {
+    return res.status(404).json({
+      error: "The user with the given ID was not found",
+    });
+  }
+  users.splice(userIndex, 1);
+  res.status(204).json({message: "The user has been deleted"});
 
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
